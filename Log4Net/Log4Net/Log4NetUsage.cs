bdfae709ac0108939d41e3200afd6c1f341b2486
qaz <qaz123>
2013-05-17 19:54:57 +0300

    dev tools homework

---
+++ Log4Net/Log4Net/Log4NetUsage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+
+namespace Log4NetTest
+{
+    class Log4NetUsage
+    {
+        private static readonly ILog log =
+          LogManager.GetLogger("Debug");
+        static void Main()
+        {
+            BasicConfigurator.Configure();
+
+            string hello = null;
+
+            log.Warn("Program starts to execute code.");
+            log.Debug("Line 22 executed.");
+
+            try
+            {
+                log.Info("Try parse null:");
+                int myInt = int.Parse(hello);
+            }
+            catch (ArgumentNullException ex)
+            {
+                log.Error(ex.Message);
+            }
+
+            try
+            {
+                int zero = 0;
+                log.Info("Try divide integer by zero.");
+                int testDevisionByZero = 1 / zero;
+            }
+            catch (DivideByZeroException ex)
+            {
+
+                log.Error(ex.Message);
+            }
+        }
+    }
+
+}
---
+++ Log4Net/Log4Net/T4CountToTen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TextTemplate
+{
+	public class MyClass
+	{
+		public static void CountToTen()
+		{
+		Console.WriteLine("Let's count to ten!");
+		Console.WriteLine(1);
+		Console.WriteLine(2);
+		Console.WriteLine(3);
+		Console.WriteLine(4);
+		Console.WriteLine(5);
+		Console.WriteLine(6);
+		Console.WriteLine(7);
+		Console.WriteLine(8);
+		Console.WriteLine(9);
+		Console.WriteLine(10);
+		}
+	}
+}
